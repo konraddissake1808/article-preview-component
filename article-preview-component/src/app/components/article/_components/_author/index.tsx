41e29f8484009c9f3db00d6c2bbf8e99e56ccdcc
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-08-23 10:31:36 +0100

    Created the share button component

--- article-preview-component/src/app/components/article/_components/_author/index.tsx
+++ article-preview-component/src/app/components/article/_components/_author/index.tsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import Image from 'next/image'
 import avatarMichelle from '../../../../../../public/avatar-michelle.jpg'
+import ShareButton from './_compenent/_shareButton';
+
+function activeState() {
+  const shareButton = document.getElementsByTagName('ShareSvg');
+  const activeButton = 'focus:bg-desaturated-dark-blue focus:text-light-grayish-blue';
+  //shareButton.classList.add(activeButton);
+}
 
 function AuthorAndShare() {
   return (
     <div className='h-[79px] w-full flex justify-center items-center'>
-        <div className='w-10/12 flex'>
-            <div className='w-9'>
-              <div className='w-full'>
-                <Image className='w-full rounded-full' src={avatarMichelle} alt="" />
+      <div className='w-10/12 flex justify-between'>
+          <div className='w-9'>
+            <div className='w-full'>
+              <Image className='w-full rounded-full' src={avatarMichelle} alt="" />
+            </div>
+          </div>
+          <div className='w-3/5 ml-4'>
+              <div>
+                <p className='font-sans text-[13px] font-bold text-very-dark-grayish-blue'>Michelle Appleton</p>
               </div>
+              <div>
+                <p className='font-sans text-[13px] font-normal text-grayish-blue'>28 Jun 2020</p>
+              </div>
+          </div>
+          <div className='w-9 flex items-center justify-center'>
+            <div className='w-full flex items-center justify-center'>
+              <ShareButton />
             </div>
-            <div className='w-3/5 ml-4'>
-                <div>
-                  <p className='font-sans text-[13px] font-bold text-very-dark-grayish-blue'>Michelle Appleton</p>
-                </div>
-                <div>
-                  <p className='font-sans text-[13px] font-normal text-grayish-blue'>28 Jun 2020</p>
-                </div>
-            </div>
-            <div></div>
-        </div>
+          </div>
+      </div>
     </div>
   )
 }
